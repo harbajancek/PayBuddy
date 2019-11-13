19c6c2ac8a10f7e31e332790ca352e160e686e9b
Jan H <honzah70@outlook.cz>
2019-11-13 14:21:03 +0100

    classes

---
+++ ClassLibrary/Payment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    class Payment
+    {
+        public User Master { get; }
+        public string Title { get; }
+        public string Description { get; }
+        public int Amount { get; }
+        public bool IsPending { get; }
+        public bool IsPaid { get; }
+    }
+}
