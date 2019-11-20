b0f6ef87cd3cbb0cd8ba80e6325e17a34079daf4
Jan H <honzah70@gmail.com>
2019-11-20 15:33:06 +0100

    publicized data models

--- ClassLibrary/Payment.cs
+++ ClassLibrary/Payment.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
-    class Payment
+    public class Payment
     {
         public User Master { get; }
         public string Title { get; }
--- ClassLibrary/User.cs
+++ ClassLibrary/User.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
-    class User
+    public class User
     {
         public string Id { get; }
         public string Nick { get; }
