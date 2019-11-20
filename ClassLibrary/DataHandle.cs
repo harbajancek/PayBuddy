f69760267d04e39961a74dbb901785ff6989e5ef
Jan H <honzah70@gmail.com>
2019-11-20 16:07:33 +0100

    preparation of methods for handle of data

--- ClassLibrary/DataHandle.cs
+++ ClassLibrary/DataHandle.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace ClassLibrary
 {
@@ -11,12 +12,91 @@ namespace ClassLibrary
     {
         static HttpClient client = new HttpClient();
         static string urlAddress = "https://hynekma16.sps-prosek.cz/PayBuddy/index.php";
-        public static void GerUser(int id,string nick,string email,string password)
+        public static async Task<User> Login(string email, string password)
         {
-            string urlToUse = urlAddress + "?action=show";
-            if()
+            string request = urlAddress + "?somethingsomething";
+            string response = await RequestApi(request);
+
+            /*
+            
+            TODO
+            somehow get user or when credentials are false return null
+
+            */
+
+            User loggedUser = new User();
+            return loggedUser;
         }
 
+        public static async Task<bool> Register(string email, string password, string nick)
+        {
+            string request = urlAddress + "?somethingsomething";
+            string response = await RequestApi(request);
+
+            /*
+            
+            TODO
+            somehow get if registration successfull
+
+            */
+
+            return false;
+        }
+
+        public static async Task<IEnumerable<User>> GetFriends(int userId)
+        {
+            string request = urlAddress + "?somethingsomething";
+            string response = await RequestApi(request);
+
+            /*
+            
+            TODO
+            somehow get all friends user
+
+            */
+
+            List<User> users = new List<User>();
+            return users;
+        }
+
+        public static async Task<IEnumerable<Payment>> GetOwnedPayments(int userID)
+        {
+            string request = urlAddress + "?somethingsomething";
+            string response = await RequestApi(request);
+
+            /*
+            
+            TODO
+            somehow get all payments which the user is master of
+
+            */
+
+            List<Payment> payments = new List<Payment>();
+            return payments;
+        }
+
+        public static async Task<IEnumerable<Payment>> GetRecievedPayments(int userId)
+        {
+            string request = urlAddress + "?somethingsomething";
+            string response = await RequestApi(request);
+
+            /*
+            
+            TODO
+            somehow get all payments which the user is reciever of
+
+            */
+
+            List<Payment> payments = new List<Payment>();
+            return payments;
+        }
+
+        private static async Task<string> RequestApi(string uriRequest)
+        {
+            HttpResponseMessage response = await client.GetAsync(uriRequest);
+
+            return await response.Content.ReadAsStringAsync();
+        }
         
     }
 }
